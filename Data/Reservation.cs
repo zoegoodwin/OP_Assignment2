436c14ce5daf5eb5e03d42fb1b3c84135a544b8f
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-03-21 13:34:52 -0600

    added individual searches
    added individual searches

--- Data/Reservation.cs
+++ Data/Reservation.cs
@@ -10,12 +10,12 @@ namespace Assignment2.Data
 	class Reservation
 	{
 		private string code;
+		private string flight;
 		private string airline;
+		private string cost;
 		private string name;
 		private string citizenship;
-		private string cost;
-		private string resCode;
-		private Flight flight;
+		private string status;
 
 		//class constructor REDO THIS
 		//public Reservation(int aReservationCode, string aAirline, string aTravelerFullName)
@@ -24,11 +24,30 @@ namespace Assignment2.Data
 		//	this.airline = aAirline;
 		//	this.name = aTravelerFullName;
 		//}
+		public Reservation(string code, string flight, string airline, string cost, string name, string citizenship, string status)
+		{
+			this.code = code;
+			this.flight = flight;
+			this.airline = airline;
+			this.cost = cost;
+			this.name = name;
+			this.citizenship = citizenship;
+			this.status = status;
+		}
 
 		//// getters and setters
 		//public int ReservationCode { get {  return reservationCode; } set { reservationCode = value; } }
 		//public string Airline { get {  return airline; } set { airline = value; } }
 		//public string TravelerFullName { get {  return name; } set { name = value; } }
+
+		public string Code { get { return code; } }
+		public string Flight { get { return flight; } }
+		public string Airline { get {  return airline; } }
+        public string Cost { get { return cost; } }
+        public string Name { get { return name; }}
+		public string Citizenship { get {  return citizenship; }}
+		public string Status { get { return status; }}
+
 
 
 	}
--- Data/ReservationManager.cs
+++ Data/ReservationManager.cs
@@ -11,15 +11,56 @@ namespace Assignment2.Data
 		//location of reservation,
 		public const string reservation_binary = "res/reservation.bin";//or in text form, easier to read
 		
-		private List<Reservation> reservations;
+        public static List<Reservation> rList = new List<Reservation>();
 
-		//make a res; param flight to book, param name of person, param citizenship; return created res. public
+        //Dummy information for reservations 
+        //Reservation reserve1 = new Reservation("G1234", "YYC", "Smith", "Canadian","599.99","TB-4017");
+        //Reservation reserve2 = new Reservation("B1234", "YEG", "James", "Canadian", "570.99", "OA-8724");
+        //Reservation reserve3 = new Reservation("C1234", "ATL", "Lenord", "Canadian", "250.99", "SL-1151");
+
+        public static void populateLists()
+        {
+            Reservation reserve1 = new Reservation("G1234", "TB-4017", "YYC", "599.99", "Smith", "Canadian", "Active");
+            Reservation reserve2 = new Reservation("B1234", "OA-8724", "YEG", "599.99", "James", "Canadian", "Active");
+            Reservation reserve3 = new Reservation("C1234", "SL-1151", "ATL", "599.99", "Lenord", "Canadian", "Inactive");
+
+            rList.Add(reserve1);
+            rList.Add(reserve2);
+            rList.Add(reserve3);
+        }
+
+        public ReservationManager()
+        {
+            populateLists();
+        }
+
+        public static List<Reservation> GetReservations()
+        {
+            return rList;
+        }
+
+        public static List<Reservation> revertList()
+        {
+            rList.Clear();
+            populateLists();
+            return rList = GetReservations();
+        }
+
+        //make a res; param flight to book, param name of person, param citizenship; return created res. public
+        public static Reservation createRes(string flight, List<Flight> fList, string personName, string citizenship)
+        {
+            //Getting flight information
+            return null;
+
+        }
 
 
-		//find the res by airline or code; param resCode, param airline, param name of person; return res objects. use tostring method
+        //find the res by airline or code; param resCode, param airline, param name of person; return res objects. use tostring method
 
 
-		//find the res by code; param resCode, return res object
 
-	}
+
+        //find the res by code; param resCode, return res object
+
+    }
 }
